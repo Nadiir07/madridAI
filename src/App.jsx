088e185c2348cb04afc72cce92f43a46adc1da5e
Nadiir07 <nadir.97@hotmail.com>
2024-10-10 19:26:57 +0200

    form is creatred

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ function App() {
   return (
     <div className='main-container'>
       <h1>
-        titi
+        AI generated Q&A
       </h1>
+
+      <div className='form-container'>
+      <label>Enter your Query: </label>
+
+      <input className='query-input' type="text" placeholder='type here' />
+
+      </div>
 
     </div>
   )
